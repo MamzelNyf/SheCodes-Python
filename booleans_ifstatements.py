b69ef61ad9467baf6263c0a1c4e0b538a3e2c049
MamzelNyf <fanny@shedesign.dev>
2020-06-28 20:53:14 +0800

    Create booleans_ifstatements.py

---
+++ booleans_ifstatements.py
@@ -0,0 +1,138 @@
+#Q1
+moths_in_house = True
+if moths_in_house:
+    print("Get the moths!")
+else:
+    print("No threats detected.")
+
+moths_in_house = False
+if moths_in_house:
+    print("Get the moths!")
+else:
+    print("No threats detected.")
+
+print()
+
+#Q2
+moths_in_house = True
+mitch_is_home = True
+
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the  moths")
+elif moths_in_house:
+    print("Meooooooooooooow! Hissssss!")
+elif mitch_is_home:
+    print("Climb on Mitch.")
+else:
+    print("No threats detected.")
+
+moths_in_house = False
+mitch_is_home = False
+
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the  moths")
+elif moths_in_house:
+    print("Meooooooooooooow! Hissssss!")
+elif mitch_is_home:
+    print("Climb on Mitch.")
+else:
+    print("No threats detected.")
+
+moths_in_house = True
+mitch_is_home = False
+
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the  moths")
+elif moths_in_house:
+    print("Meooooooooooooow! Hissssss!")
+elif mitch_is_home:
+    print("Climb on Mitch.")
+else:
+    print("No threats detected.")
+
+moths_in_house = False
+mitch_is_home = True
+
+if moths_in_house and mitch_is_home:
+    print("Hooman! Help me get the moths")
+elif moths_in_house:
+    print("Meooooooooooooow! Hissssss!")
+elif mitch_is_home:
+    print("Climb on Mitch.")
+else:
+    print("No threats detected.")
+
+print()
+
+#Q3
+light_colour = "Red"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Red"
+car_detected = True
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Green"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Green"
+car_detected = True
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Amber"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Amber"
+car_detected = True
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+print()
+
+#Q4
+height = 120
+
+if height >= 120:
+    print("Hop on!")
+else: 
+    print("Sorry, not today :(")
+
+height = 50
+
+if height >= 120:
+    print("Hop on!")
+else: 
+    print("Sorry, not today :(")
+
+height = 191
+
+if height >= 120:
+    print("Hop on!")
+else: 
+    print("Sorry, not today :(")
