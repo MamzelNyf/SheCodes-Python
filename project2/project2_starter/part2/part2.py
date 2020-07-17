dbaa0f465843588fb18d4b893b2c62839a385563
MamzelNyf <fanny@shedesign.dev>
2020-07-17 18:34:24 +0800

    install virtual environment

--- project2/project2_starter/part2/part2.py
+++ project2/project2_starter/part2/part2.py
@@ -1,2 +1,10 @@
 import json
 import plotly.express as px
+
+def graph(forecast_file):
+    with open(forecast_file) as json_file:
+        forecast_data = json.load(json_file)
+
+        
+if __name__ == "__main__":
+    print(graph("data/forecast_5days_b.json"))
